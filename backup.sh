24d5effdf5391602af8e0fa078b57a2e8b1f6b8e
Adith Radiyo <adith@zenstealth.com>
2012-07-05 15:54:58 -0400

    Now backs up ssh config

--- backup.sh
+++ backup.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 # Script to backup dotfiles
 
+# Profiles
 cp ~/.profile ~/Dropbox/Code/dotfiles/bash/.profile
 cp ~/.zshrc ~/Dropbox/Code/dotfiles/zsh/.zshrc
 
-cp /etc/motd ~/Dropbox/Code/dotfiles/bash/motd
+# SSH
+cp -r ~/.ssh/* ~/Dropbox/Code/dotfiles/ssh/
 
+# Message of the day
+cp /etc/motd ~/Dropbox/Code/dotfiles/bash/motd
+# Vim
 cp ~/.vimrc  ~/Dropbox/Code/dotfiles/vim/.vimrc
 cp -r ~/.vim/* ~/Dropbox/Code/dotfiles/vim/.vim/
