e1e66692c67c173565c2a60ccae75c51226dd7c5
Adith Radiyo <adith@zenstealth.com>
2012-09-29 14:27:40 -0400

    Updated backup script

--- backup.sh
+++ backup.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-# Script to backup dotfiles
+# Script to backup Dotfiles
 
 # Profiles
-cp ~/.profile ~/Dropbox/Code/dotfiles/bash/.profile
-cp ~/.zshrc ~/Dropbox/Code/dotfiles/zsh/.zshrc
+cp ~/.profile ~/Dropbox/Code/Dotfiles/bash/.profile
+cp ~/.zshrc ~/Dropbox/Code/Dotfiles/zsh/.zshrc
 
 # SSH
-cp -r ~/.ssh/* ~/Dropbox/Code/dotfiles/ssh/
+cp -r ~/.ssh/* ~/Dropbox/Code/Dotfiles/ssh/
 
 # Message of the day
-cp /etc/motd ~/Dropbox/Code/dotfiles/bash/motd
+cp /etc/motd ~/Dropbox/Code/Dotfiles/bash/motd
 
 # Git
-cp ~/.gitignore ~/Dropbox/Code/dotfiles/git/.gitignore
+cp ~/.gitignore ~/Dropbox/Code/Dotfiles/git/.gitignore
 
 # Vim
-cp ~/.vimrc  ~/Dropbox/Code/dotfiles/vim/.vimrc
-cp -r ~/.vim/* ~/Dropbox/Code/dotfiles/vim/.vim/
+cp ~/.vimrc  ~/Dropbox/Code/Dotfiles/vim/.vimrc
+cp -r ~/.vim/* ~/Dropbox/Code/Dotfiles/vim/.vim/
