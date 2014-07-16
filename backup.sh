d75ca3b3c323ad57b4a0f1340b1d69ce989d1321
Adith Radiyo <adith@zenstealth.com>
2014-07-16 18:52:11 -0400

    Update Dotfiles directory in Sync folder

--- backup.sh
+++ backup.sh
@@ -3,17 +3,17 @@
 
 # Profiles
 # UNUSED: cp ~/.profile ~/Sync/Code/Dotfiles/bash/.profile
-cp ~/.zshrc ~/Sync/Code/Dotfiles/zsh/.zshrc
+cp ~/.zshrc ~/Sync/Tech/Code/Dotfiles/zsh/.zshrc
 
 # SSH
-cp -r ~/.ssh/* ~/Sync/Code/Dotfiles/ssh/
+cp -r ~/.ssh/* ~/Sync/Tech/Code/Dotfiles/ssh/
 
 # Message of the day
-cp /etc/motd ~/Sync/Code/Dotfiles/bash/motd
+cp /etc/motd ~/Sync/Tech/Code/Dotfiles/bash/motd
 
 # Git
-cp ~/.gitignore ~/Sync/Code/Dotfiles/git/.gitignore
+cp ~/.gitignore ~/Sync/Tech/Code/Dotfiles/git/.gitignore
 
 # Vim
-cp ~/.vimrc  ~/Sync/Code/Dotfiles/vim/.vimrc
-cp -r ~/.vim/* ~/Sync/Code/Dotfiles/vim/.vim/
+cp ~/.vimrc  ~/Sync/Tech/Code/Dotfiles/vim/.vimrc
+cp -r ~/.vim/* ~/Sync/Tech/Code/Dotfiles/vim/.vim/
